46863f6af79f54f1ec3337ff47a74fce959a61db
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2025-01-03 17:33:37 +0000

    Initial version from start.spring.io

---
+++ src/main/java/org/dontpanic/jackson_stream/JacksonStreamApplication.java
@@ -0,0 +1,13 @@
+package org.dontpanic.jackson_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dontpanic/jackson_stream/JacksonStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dontpanic.jackson_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
